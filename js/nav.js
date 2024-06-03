386b35ec5d80ff059df48f5c3fb7e278d8f1dcd2
MiguelAngelMadroneroSequeira <miguel.madronero@genstudents.org>
2024-06-02 20:29:42 -0500

    animacion de cerrar abrir agregadas

--- js/nav.js
+++ js/nav.js
@@ -2,27 +2,24 @@ const nav = document.querySelector('.nav__links');
 const botonAbrir = document.querySelector('#nav__open');
 const botonAbrirMenu = document.querySelector('.header__menu--container')
 const botonCerrar = document.querySelector('#nav__close');
-const path = document.getElementById('nav__open--path');
+
 
 botonAbrirMenu.addEventListener("click", ()=>{
     nav.classList.add('nav__links--mostrar');
-    console.log(botonAbrir);
-    console.log(nav);
+   
 });
 
-// path.addEventListener("click", ()=>{
-//     nav.classList.add('nav__links--mostrar');
-    
-//     nav.style.opacity="1";
-//     nav.style.visibility= "visible";
-//     console.log(path);
-//     console.log(nav);
-// })
+
 
 botonCerrar.addEventListener("click", ()=>{
     nav.classList.remove("nav__links--mostrar");
-    // nav.style.opacity="0";
-    // nav.style.visibility= "hidden";
+
+    nav.classList.add("nav__links--close");
+
+    setTimeout(() => {
+     nav.classList.remove("nav__links--close");
+    }, 500);
+    
 })
 
 document.addEventListener("click", function(event) {
