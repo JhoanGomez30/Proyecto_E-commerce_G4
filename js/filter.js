3c44a5ed8bf3ddbda9d103112eef5fe96f1a6d90
LAQuinteroGGen <laura.quintero@genstudents.org>
2024-06-15 15:36:56 -0500

    cambios en filtro superior, pagina productos

--- js/filter.js
+++ js/filter.js
@@ -6,3 +6,26 @@ function toggleFilterBar() {
     filterBar.style.display = "flex";
   }
 }
+
+// const prev = document.querySelector('.topFilter__prev')
+// const next = document.querySelector('.topFilter__next')
+// const topFilter__paragraph = document.querySelector('.topFilter__paragraph')
+
+// prev.addEventListener('click', () => {
+//   topFilter__paragraph.scrollLeft
+// })
+
+// ____________________________________________---
+// const figure = document.querySelector('.topFilter__figure');
+// const optionsContainer = document.querySelector('.topFilter__paragraph');
+
+// figure.addEventListener('click', (event) => {
+//   const scrollStep = 200; // Valor de desplazamiento horizontal
+//   if (event.clientX < figure.getBoundingClientRect().left) {
+//     // Click a la izquierda de topFilter__figure
+//     optionsContainer.scrollLeft -= scrollStep;
+//   } else {
+//     // Click a la derecha de topFilter__figure
+//     optionsContainer.scrollLeft += scrollStep;
+//   }
+// });
