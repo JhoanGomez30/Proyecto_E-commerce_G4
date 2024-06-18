fe08c52247dbc72a3ca1797fe2b1f0ce235a6fdc
LAQuinteroGGen <laura.quintero@genstudents.org>
2024-06-17 21:05:31 -0500

    Actualización en tarjetas de equipo, página nosotros y actualización en filtro superior página productos

--- js/filter.js
+++ js/filter.js
@@ -1,12 +1,3 @@
-// function toggleFilterBar() {
-//   let filterBar = document.querySelector(".filterBar");
-//   if (filterBar.style.display === "flex") {
-//     filterBar.style.display = "none";
-//   } else {
-//     filterBar.style.display = "flex";
-//   }
-// }
-
 function toggleFilterBar() {
   let filterBar = document.querySelector(".filterBar");
  filterBar.classList.toggle("filterBar--active")
@@ -26,8 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
   
   rightScrollButton.addEventListener("click", function() {
     topFilterContent.scrollBy({
-      left: 150, // Ajusta el valor según sea necesario para el desplazamiento
-      behavior: "smooth" // Desplazamiento suave
+      left: 150,
+      behavior: "smooth"
     });
   })
 })
