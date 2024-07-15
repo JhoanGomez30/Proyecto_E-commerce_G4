5c65da2e8dce753c4debcd2ca1714a5e188ba07a
LAQuinteroGGen <laura.quintero@genstudents.org>
2024-07-15 08:37:01 -0500

    Actualizacion seccion home

---
+++ js/seccionPreguntas.js
@@ -0,0 +1,53 @@
+const preguntas = document.querySelectorAll('.preguntas .contenedor-pregunta');
+preguntas.forEach((pregunta) => {
+	pregunta.addEventListener('click', (e) => {
+		e.currentTarget.classList.toggle('activa');
+
+		const respuesta = pregunta.querySelector('.respuesta');
+		const alturaRealRespuesta = respuesta.scrollHeight;
+		
+		if(!respuesta.style.maxHeight){
+			// Si esta vacio el maxHeight entonces ponemos un valor.
+			respuesta.style.maxHeight = alturaRealRespuesta + 'px';
+		} else {
+			respuesta.style.maxHeight = null;
+		}
+
+		// [Opcional] Reiniciamos las demas preguntas
+		preguntas.forEach((elemento) => {
+			// Solamente queremos ejecutar el codigo para las preguntas que no 
+			// sean la pregunta a la que le dimos click.
+			if(pregunta !== elemento){
+				elemento.classList.remove('activa');
+				elemento.querySelector('.respuesta').style.maxHeight = null;
+			}
+		});
+
+
+	});
+});
+
+const categorias = document.querySelectorAll('#categorias .categoria');
+const contenedorPreguntas = document.querySelectorAll('.contenedor-preguntas');
+let categoriaActiva = null;
+
+categorias.forEach((categoria) => {
+	categoria.addEventListener('click', (e) => {
+		categorias.forEach((elemento) => {
+			elemento.classList.remove('activa');
+		});
+
+		e.currentTarget.classList.toggle('activa');
+		categoriaActiva = categoria.dataset.categoria;
+
+
+		// Activamos el contenedor de preguntas que corresponde
+		contenedorPreguntas.forEach((contenedor) => {
+			if(contenedor.dataset.categoria === categoriaActiva){
+				contenedor.classList.add('activo');
+			} else {
+				contenedor.classList.remove('activo');
+			}
+		});
+	});
+});
